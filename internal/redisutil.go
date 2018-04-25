73f6be8326e7d781b753a5e0f112fc17cabef572
pzrr@qq.com <pzrr@qq.com>
2018-04-25 21:21:06 +0800

    #### Version 0.5 * RedisCache新增ZRange、ZRevRange、ZCard、HMGet接口 * 2018-04-25 20:00

--- internal/redisutil.go
+++ internal/redisutil.go
@@ -212,11 +212,11 @@ func (rc *RedisClient) HGet(hashID string, field string) (string, error) {
 }
 
 // HMGet 返回 key 指定的哈希集中指定字段的值
-func (rc *RedisClient) HMGet(hashID string, field ...interface{}) ([]string, error) {
+func (rc *RedisClient) HMGet(hashID string, field ...interface{}) (interface{}, error) {
 	conn := rc.pool.Get()
 	defer conn.Close()
 	args := append([]interface{}{hashID}, field...)
-	reply, err := redis.Strings(conn.Do("HGETALL", args))
+	reply, err := conn.Do("HGETALL", args)
 	return reply, err
 }
 
