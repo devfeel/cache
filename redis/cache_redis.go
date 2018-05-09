376b7ffbaef4e09d6db80a243923eec06c32d85a
pzrr@qq.com <pzrr@qq.com>
2018-05-09 18:57:47 +0800

    #### Version 0.6 * 新增GetRedisCachePoolConf接口，用于需要设置连接池配置的场景 * 默认GetRedisCache与GetCache使用redis时，连接池设置默认使用RedisConnPool_MaxIdle, RedisConnPool_MaxActive * 2018-05-09 18:00

--- redis/cache_redis.go
+++ redis/cache_redis.go
@@ -416,5 +416,5 @@ func (ca *redisCache) ZRevRange(key string, start, stop int64)([]string, error){
 // EVAL used to evaluate scripts using the Lua interpreter built into Redis starting from version 2.6.0
 func (ca *redisCache) EVAL(script string, argsNum int, arg ...interface{})(interface{}, error){
 	client := internal.GetRedisClient(ca.serverUrl, ca.maxIdle, ca.maxActive)
-	return client.EVAL(script, argsNum, arg...)
+	return client.EVAL(script, argsNum, arg[0:])
 }
